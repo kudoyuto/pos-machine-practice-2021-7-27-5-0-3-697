a48aa188b32082cbdf9ef43c5c81df6631c47eec
kudoyuto <yuto.kudo@oocl.com>
2021-08-04 01:16:37 +0800

    [PVOSV2][YutoKudo]feat: Load all items info

---
+++ src/main/java/pos/machine/Main.java
@@ -0,0 +1,13 @@
+package pos.machine;
+
+import javafx.geometry.Pos;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        PosMachine.loadAllItemsInfo(ItemDataLoader.loadBarcodes());
+
+    }
+}
--- src/main/java/pos/machine/PosMachine.java
+++ src/main/java/pos/machine/PosMachine.java
@@ -6,4 +6,21 @@ public class PosMachine {
     public String printReceipt(List<String> barcodes) {
         return null;
     }
+
+    public static List<ItemInfo> loadAllItemsInfo(List<String> barcodes) {
+        List<ItemInfo> allItemDetails = ItemDataLoader.loadAllItemInfos();
+        ;
+        for(int i=0; i<allItemDetails.size(); i++){
+
+
+                System.out.println("Name: " + allItemDetails.get(i).getName() +","+"Unit Price: "+allItemDetails.get(i).getPrice()+ "(yuan)" );
+
+
+
+
+        }
+        return allItemDetails;
+    }
 }
+
+
